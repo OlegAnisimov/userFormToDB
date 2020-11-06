0f2cbeeab93099890c5f0daad9d573dfbaab67d7
hohoho <gramafon@yandex.ru>
2020-11-07 02:46:56 +0300

    get input[name] to DB work of checking
    ? timestamp not work in DB

--- send.php
+++ send.php
@@ -1,24 +1,35 @@
 <?php
-echo "hello";
 $dsn = "mysql:host=localhost;dbname=test;charset=utf8";
 $dbuser = "root";
 $dbpass = "mysql";
-
-$connection = new PDO($dsn, $dbuser, $dbpass);
-$connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-if (isset($_POST['name'])) {
-    $name = strip_tags($_POST['name']);
-    $name = htmlentities($_POST['name']);
-    $sql = "INSERT INTO form(name) VALUES ('$name')";
-    $connection->query($sql);
-} else {
-    echo
-    <<<INFO
-        <div class="info">Заполнить поле Имя</div>
-INFO;
-
+$arr = []; //?
+try {
+    $connection = new PDO($dsn, $dbuser, $dbpass);
+    $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    if (isset($_POST['name'])) {
+        $name = strip_tags($_POST['name']);
+        $name = htmlentities($_POST['name']);
+        $sqlCheckQuery = "SELECT name FROM form WHERE name = '$name'";
+        $check         = $connection->prepare($sqlCheckQuery);
+        $check->execute([$name]);
+        $condition = $check->fetchColumn();
+        if (gettype($condition) === "string") { // наличие запсис
+            exit("order exist");
+        } else
+            setcookie('name', $name);
+            $sql_insert = "INSERT INTO form(name) VALUES ('$name')";
+            $connection->query($sql_insert);
+            mail('jrrtolkin@mail.ru', 'form', "dgdsgdsg");
+    }} catch (PDOException $e) {
+    echo $e->getMessage("BD problems");
 }
+
+
+
+
+
+
+
 
 
 
